4bcc644ae549727e48a0bac093d614cc2d8ae409
jaxmandev <39777721+jaxmandev@users.noreply.github.com>
2020-11-06 20:58:05 +0000

    Updated pseudo code

--- q2-3.py
+++ q2-3.py
@@ -1,6 +1,8 @@
-#Q2: create a loop with a range that asks for names and appends to list_names
+# Q2: create a loop with a range that asks for names and appends to list_names
+# ask for input to set loop iteration count
 body_count = int(input("How many people are present? "))
 list_names = []
+# loop will append input into list_names
 for j in range(body_count):
     print('Enter a name')
     name1 = str(input())
@@ -8,12 +10,10 @@ for j in range(body_count):
 print(list_names)
 
 
-#Q3: make a loop that iterated over each name in list_name and
+# Q3: make a loop that iterated over each name in list_name and
 # format's it into lowercase in a new variable called list_names_lower
 
 list_names_lower = []
 for a in list_names:
     list_names_lower.append(a.lower())
 print(list_names_lower)
-
-#Q4: Iterate over the list of values to find if the are even
